84665d4fe1d269f4cb51ec0cb388d47a3084c5cb
mipe102 <mipe102@users.noreply.github.com>
2014-11-01 19:56:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+
+<html>
+<head>
+	<meta name="viewport" content="width=device-width" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!--        <meta HTTP-EQUIV="REFRESH" content="2; url=http://www.mipe.dk" /> -->  	
+<title>Redirect</title>
+</head>
+<body>
+<p>Now index.php file on github</p>
+<?php
+echo “<p>This is a PHP line</p>”;
+phpinfo();
+?>
+
 <?php
 //$path_parts = pathinfo('/home/pi/RPi_sensor/SensorData.db');
 
@@ -13,15 +28,5 @@ while ($row = $results->fetchArray()) {
 	var_dump($row);
 }
 ?>
-<html>
-<head>
-	<meta name="viewport" content="width=device-width" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<!--        <meta HTTP-EQUIV="REFRESH" content="2; url=http://www.mipe.dk" /> -->  	
-<title>Redirect</title>
-</head>
-<body>
-<p>Now index.php file on github</p>
-
 </body>
 </html>
