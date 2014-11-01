27038abd4bbbd3866ed985076904e0181be25c7b
mipe102 <mipe102@users.noreply.github.com>
2014-11-01 20:02:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <html>
 <head>
 	<meta name="viewport" content="width=device-width" />
@@ -11,22 +10,6 @@
 <?php
 echo “<p>This is a PHP line</p>”;
 phpinfo();
-?>
-
-<?php
-//$path_parts = pathinfo('/home/pi/RPi_sensor/SensorData.db');
-
-//echo $path_parts['dirname'], "\n";
-//echo $path_parts['basename'], "\n";
-//echo $path_parts['extension'], "\n";
-//echo $path_parts['filename'], "\n"; // since PHP 5.2.0
-//$db = new SQLite3('/home/pi/RPi_sensor/SensorData.db');
-$db = new SQLite3('SensorData.db');
-
-$results = $db->query('SELECT * FROM onewire');
-while ($row = $results->fetchArray()) {
-	var_dump($row);
-}
 ?>
 </body>
 </html>
